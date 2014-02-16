4283c3e8e03962bc9f0c1e6839a0908afc71bf91
AE Creations <aecreations@server.fake>
2014-02-16 01:18:37 -0800

    Added constant for the extension's ID

--- src/modules/aeConstants.js
+++ src/modules/aeConstants.js
@@ -27,6 +27,9 @@ const EXPORTED_SYMBOLS = ["aeConstants"];
 
 
 var aeConstants = {
+  // Extension GUID
+  EXTENSION_ID: "{91aa5abe-9de4-4347-b7b5-322c38dd9271}",
+
   // Host app GUIDs
   HOSTAPP_FX_GUID: "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}",
   HOSTAPP_TB_GUID: "{3550f703-e582-4d05-9a08-453d09bdfdc6}",
