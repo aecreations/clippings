c3928523fe1b440a7c6dab602dfbb51c413f45b6
AE Creations <aecreations@server.fake>
2014-12-20 23:29:39 -0800

    Renamed XUL file for Clippings Manager window

--- src/chrome/content/tbMessengerOverlay.js
+++ src/chrome/content/tbMessengerOverlay.js
@@ -142,7 +142,7 @@ window.aecreations.clippings = {
 
   openClippingsManager: function () 
   {
-    var wnd = window.open("chrome://clippings/content/clippings.xul",
+    var wnd = window.open("chrome://clippings/content/clippingsMgr.xul",
 			  "clippings_wndobj", "chrome,resizable");
     wnd.focus();
   },
