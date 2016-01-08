84a4fe0e80d22253a5b3a8d5bef546f49dfac813
AE Creations <aecreations@server.fake>
2016-01-07 22:34:29 -0800

    Refactoring in preparation for using message manager in multi-process Firefox.  Renamed Firefox overlay JS file to make it clear that it is a chrome script (as opposed to a frame script)

--- src/chrome/content/fxBrowserChrome.js
+++ src/chrome/content/fxBrowserChrome.js
@@ -16,7 +16,7 @@
  *
  * The Initial Developer of the Original Code is 
  * Alex Eng <ateng@users.sourceforge.net>.
- * Portions created by the Initial Developer are Copyright (C) 2005-2015
+ * Portions created by the Initial Developer are Copyright (C) 2005-2016
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
