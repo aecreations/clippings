6de968d76d3c663aae9c3e4e0415c0bbd73e6e70
AE Creations <aecreations@server.fake>
2017-05-22 22:55:42 -0700

    Initial commit of WebExtension source files

---
+++ wx-src/background.js
@@ -0,0 +1,224 @@
+/* -*- mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Clippings.
+ *
+ * The Initial Developer of the Original Code is 
+ * Alex Eng <ateng@users.sourceforge.net>.
+ * Portions created by the Initial Developer are Copyright (C) 2005-2017
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+let db = null;
+
+/***
+chrome.contextMenus.create({
+  id: "clipping-1",
+  title: "New Clipping 1",
+  contexts: ["editable"]
+});
+
+chrome.contextMenus.create({
+  id: "clipping-2",
+  title: "New Clipping 2",
+  contexts: ["editable"]
+});
+
+// Test nested menus
+var parentMnuID = chrome.contextMenus.create({
+  id: "folder-menu-1",
+  title: "Folder A",
+  contexts: ["editable"]
+});
+
+chrome.contextMenus.create({
+  id: "clipping-A1",
+  title: "New Clipping A1",
+  parentId: parentMnuID,
+  contexts: ["editable"]
+});
+
+chrome.contextMenus.create({
+  id: "clipping-A2",
+  title: "New Clipping A2",
+  parentId: parentMnuID,
+  contexts: ["editable"]
+});
+
+var parentSubmnuID = chrome.contextMenus.create({
+  id: "subfolder-AA",
+  title: "Folder AA",
+  parentId: parentMnuID,
+  contexts: ["editable"]
+});
+
+chrome.contextMenus.create({
+  id: "clipping-A3",
+  title: "New Clipping A3",
+  parentId: parentMnuID,
+  contexts: ["editable"]
+});
+
+chrome.contextMenus.create({
+  id: "clipping-AA-1",
+  title: "New Clipping AA1",
+  parentId: parentSubmnuID,
+  contexts: ["editable"]
+});
+***/
+
+
+//
+// Browser window and Clippings menu initialization
+//
+
+function init()
+{
+  db = new Dexie("aeClippings");
+  db.version(1).stores({
+    clippings: "++id, name, parentFolderID"
+  });
+  db.open().catch(e => {
+    console.error("Clippings/wx: Error opening database: " + e);
+  });
+
+  chrome.runtime.getPlatformInfo(aInfo => { console.log("Clippings/wx: OS: " + aInfo.os); });
+
+  // Clippings context menu items.
+  chrome.contextMenus.create({
+    id: "ae-clippings-new",
+    title: "New...",
+    contexts: ["editable", "selection"]
+  });
+
+  chrome.contextMenus.create({
+    id: "ae-clippings-manager",
+    title: "Organize Clippings",
+    contexts: ["editable", "selection"]
+  });
+
+  db.clippings.count().then(aResult => {
+    console.log("Number of clippings at root folder level: " + aResult);
+    let numClippingsInRoot = aResult;
+    if (numClippingsInRoot > 0) {
+      chrome.contextMenus.create({ type: "separator", contexts: ["editable"]});
+
+      db.clippings.where("parentFolderID").equals(0).each((aItem, aCursor) => {
+          chrome.contextMenus.create({
+            id: "ae-clippings-clipping-" + aItem.id,
+            title: aItem.name,
+            contexts: ["editable"]
+          });
+      });
+    }
+  });  
+}
+
+
+function createClipping(aName, aContent/*, aShortcutKey, aSrcURL */)
+{
+  db.clippings.add({name: aName, content: aContent, shortcutKey: "", parentFolderID: 0})
+    .then(aID => {
+      console.log("Successfully created new clipping!\nid = " + aID);
+      db.clippings.get(aID)
+        .then(aResult => {
+          console.log("Name: " + aResult.name + "\nText: " + aResult.content);
+      });
+    })
+    .catch(e => { console.log("Error: " + e)});
+}
+
+
+//
+// Click event listener for the context menu items
+//
+
+chrome.contextMenus.onClicked.addListener((aInfo, aTab) => {
+  switch (aInfo.menuItemId) {
+  case "ae-clippings-new":
+    let text = aInfo.selectionText;  // Line breaks are NOT preserved!
+    if (! text) {
+      window.alert("No text was selected.  Please select text first.");
+      break;
+    }
+
+    let name = window.prompt("New clipping name:", "New Clipping");
+    if (! name) {
+      return;
+    }
+
+    chrome.tabs.query({ active: true, currentWindow: true }, aTabs => {
+      if (! aTabs[0]) {
+        window.alert("Please return to the browser window and try again.");
+        return;
+      }
+
+      let activeTabID = aTabs[0].id;
+      console.log("Clippings/wx: Extension sending message 'ae-clippings-new' to content script; active tab ID: " + activeTabID);
+
+      chrome.tabs.sendMessage(activeTabID, { msgID: "ae-clippings-new" }, null, aResp => {
+        let content = aResp.content;
+        if (! content) {
+          console.log("Clippings/wx: Content script was unable to retrieve content from inside an HTML frame.  Retrieving selection text from context menu info.");
+          content = text;
+        }
+        console.log("Clippings/wx: Create clipping from selected text:\nName: " + name + "\nText: " + content);
+
+        createClipping(name, content);
+      });
+    });
+    
+    break;
+
+  case "ae-clippings-manager":
+    console.log("TO DO: Open Clippings Manager");
+    break;
+
+  default:
+    if (aInfo.menuItemId.startsWith("ae-clippings-clipping-")) {
+      let clippingID = Number(aInfo.menuItemId.substr(22));
+      db.clippings.where("id").equals(clippingID).first(aClipping => {
+        if (! aClipping) {
+          window.alert("Cannot find clipping.\nClipping ID = " + clippingID);
+          return;
+        }
+        console.log("Pasting clipping named \"" + aClipping.name + "\"\nid = " + aClipping.id);
+
+        chrome.tabs.query({ active: true, currentWindow: true }, aTabs => {
+          if (! aTabs[0]) {
+            // This should never happen...
+            window.alert("Unable to paste clipping because there is no active browser tab.");
+            return;
+          }
+
+          let activeTabID = aTabs[0].id;
+          let msgParams = {
+            msgID: "ae-clippings-paste",
+            content: aClipping.content
+          };
+          chrome.tabs.sendMessage(activeTabID, msgParams, null, aResp => {
+            console.log("It is " + aResp + " that the clipping was successfully pasted.");
+          });
+        });        
+      });
+    }
+    break;
+  }
+});
+
+
+init();
---
+++ wx-src/content.js
@@ -0,0 +1,193 @@
+/* -*- mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Clippings.
+ *
+ * The Initial Developer of the Original Code is 
+ * Alex Eng <ateng@users.sourceforge.net>.
+ * Portions created by the Initial Developer are Copyright (C) 2016-2017
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+
+function handleRequestNewClipping(aRequest)
+{
+  let rv = {};
+  let activeElt = window.document.activeElement;
+
+  console.log("Clippings/wx: content.js::handleRequestNewClipping(): activeElt = " + (activeElt ? activeElt.toString() : "???"));
+
+  if (isElementOfType(activeElt, "HTMLInputElement")
+      || isElementOfType(activeElt, "HTMLTextAreaElement")) {
+    // <input type="text"> or <textarea>
+    let text = "";
+
+    if (activeElt.selectionStart == activeElt.selectionEnd) {
+      text = activeElt.value;
+    }
+    else {
+      text = activeElt.value.substring(activeElt.selectionStart, 
+                                       activeElt.selectionEnd);
+    }
+      
+    rv.content = text;
+  }
+  else if (isElementOfType(activeElt, "HTMLBodyElement")) {
+    let selection = activeElt.ownerDocument.getSelection();
+    rv.content = selection.toString();
+  }
+    
+  return rv;
+}
+
+
+function handleRequestInsertClipping(aRequest)
+{
+  let rv = false;
+  let clippingText = aRequest.content;
+  let activeElt = window.document.activeElement;
+
+  console.log("Clippings/wx: content.js::handleRequestInsertClipping(): activeElt = " + (activeElt ? activeElt.toString() : "???"));  
+
+  if (isElementOfType(activeElt, "HTMLInputElement")
+      || isElementOfType(activeElt, "HTMLTextAreaElement")) {
+    rv = insertTextIntoTextbox(activeElt, clippingText);
+  }
+  // Rich text editor
+  else if (isElementOfType(activeElt, "HTMLIFrameElement")) {
+    let doc = activeElt.contentDocument;
+    rv = insertTextIntoRichTextEditor(doc, clippingText);
+  }
+  // Rich text editor used by Gmail and Outlook.com
+  else if (isElementOfType(activeElt, "HTMLDivElement")) {
+    let doc = activeElt.ownerDocument;
+    rv = insertTextIntoRichTextEditor(doc, clippingText);
+  }
+  else {
+    rv = false;
+  }
+
+  return rv;
+}
+
+
+//
+// Helper functions
+//
+
+function isElementOfType(aElement, aTypeStr)
+{
+  let re = new RegExp(aTypeStr);
+  return (aElement.toString().search(re) != -1);
+}
+
+
+function insertTextIntoTextbox(aTextboxElt, aInsertedText)
+{
+  console.log("Clippings/wx: insertTextIntoTextbox()");
+  
+  var text, pre, post, pos;
+  text = aTextboxElt.value;
+
+  if (aTextboxElt.selectionStart == aTextboxElt.selectionEnd) {
+    var point = aTextboxElt.selectionStart;
+    pre = text.substring(0, point);
+    post = text.substring(point, text.length);
+    pos = point + aInsertedText.length;
+  }
+  else {
+    var p1 = aTextboxElt.selectionStart;
+    var p2 = aTextboxElt.selectionEnd;
+    pre = text.substring(0, p1);
+    post = text.substring(p2, text.length);
+    pos = p1 + aInsertedText.length;
+  }
+
+  aTextboxElt.value = pre + aInsertedText + post;
+  aTextboxElt.selectionStart = pos;
+  aTextboxElt.selectionEnd = pos;
+
+  return true;
+}
+
+
+function insertTextIntoRichTextEditor(aRichTextEditorDocument, aClippingText)
+{
+  console.log("Clippings/wx: insertTextIntoRichTextEditor()");
+
+  let hasHTMLTags = aClippingText.search(/<[a-z1-6]+( [a-z]+(\="?.*"?)?)*>/i) != -1;
+  let hasRestrictedHTMLTags = aClippingText.search(/<\?|<%|<!DOCTYPE|(<\b(html|head|body|meta|script|applet|embed|object|i?frame|frameset)\b)/i) != -1;
+  let clippingText = aClippingText;
+
+  if (hasHTMLTags) {
+    if (hasRestrictedHTMLTags) {
+      clippingText = clippingText.replace(/&/g, "&amp;");
+      clippingText = clippingText.replace(/</g, "&lt;");
+      clippingText = clippingText.replace(/>/g, "&gt;");
+    }
+  }
+  else {
+    // Could be plain text but with angle brackets, e.g. for denoting URLs
+    // or email addresses, e.g. <joel_user@acme.com>, <http://www.acme.com>
+    let hasOpenAngleBrackets = clippingText.search(/</) != -1;
+    let hasCloseAngleBrackets = clippingText.search(/>/) != -1;
+
+    if (hasOpenAngleBrackets) {
+      clippingText = clippingText.replace(/</g, "&lt;");
+    }
+    if (hasCloseAngleBrackets) {
+      clippingText = clippingText.replace(/>/g, "&gt;");	  
+    }
+  }
+
+  // TO DO: Get this from user pref.
+  let autoLineBreak = true;
+  
+  let hasLineBreakTags = clippingText.search(/<br|<p/i) != -1;
+  if (autoLineBreak && !hasLineBreakTags) {
+    clippingText = clippingText.replace(/\n/g, "<br>");
+  }
+
+  try {
+    aRichTextEditorDocument.execCommand("insertHTML", false, clippingText);
+  }
+  catch (e) {}
+
+  return true;
+}
+
+
+//
+// Message listeners
+//
+
+chrome.runtime.onMessage.addListener((aRequest, aSender, aSendResponse) => {
+  console.log("Clippings/wx: Content script received message '" + aRequest.msgID + "' from extension");
+
+  let resp = null;
+  
+  if (aRequest.msgID == "ae-clippings-new") {
+    resp = handleRequestNewClipping(aRequest);
+  }
+  else if (aRequest.msgID == "ae-clippings-paste") {
+    resp = handleRequestInsertClipping(aRequest);
+  }
+
+  aSendResponse(resp);
+});
+
+
