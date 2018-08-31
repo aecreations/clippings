98df3689d74cdf2bb745e0aa5d02e4d84e20ae37
AE Creations <aecreations@users.noreply.github.com>
2018-08-31 13:03:38 -0700

    Added workaround for popup window display bug

--- wx-src/pages/backup.js
+++ wx-src/pages/backup.js
@@ -35,6 +35,16 @@ $(() => {
 	}
       });
     });
+
+    // Fix for Fx57 bug where bundled page loaded using
+    // browser.windows.create won't show contents unless resized.
+    // See <https://bugzilla.mozilla.org/show_bug.cgi?id=1402110>
+    browser.windows.getCurrent(aWnd => {
+      browser.windows.update(aWnd.id, {
+	width: aWnd.width + 1,
+	focused: true,
+      });
+    });
   });
 });
 
