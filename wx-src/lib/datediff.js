d618510d71dccd0233e2a8510bfb108d925f0872
AE Creations <aecreations@users.noreply.github.com>
2019-09-04 22:30:57 -0700

    Use original source code for the Datediff library

--- wx-src/lib/datediff.js
+++ wx-src/lib/datediff.js
@@ -1,38 +1,46 @@
-// DateDiff & DateMeasure: Calculate days, hours, minutes, seconds between
-// two Dates.
-// Adapted from <https://gist.github.com/remino/1563963>
+(function() {
 
-function DateDiff(date1, date2) {
-  this.days = null;
-  this.hours = null;
-  this.minutes = null;
-  this.seconds = null;
-  this.date1 = date1;
-  this.date2 = date2;
+  function DateDiff(date1, date2) {
+    this.days = null;
+    this.hours = null;
+    this.minutes = null;
+    this.seconds = null;
+    this.date1 = date1;
+    this.date2 = date2;
 
-  this.init();
-}
+    this.init();
+  }
 
-DateDiff.prototype.init = function() {
-  var data = new DateMeasure(this.date1 - this.date2);
-  this.days = data.days;
-  this.hours = data.hours;
-  this.minutes = data.minutes;
-  this.seconds = data.seconds;
-};
+  DateDiff.prototype.init = function() {
+    var data = new DateMeasure(this.date1 - this.date2);
+    this.days = data.days;
+    this.hours = data.hours;
+    this.minutes = data.minutes;
+    this.seconds = data.seconds;
+  };
 
-function DateMeasure(ms) {
-  var d, h, m, s;
-  s = Math.floor(ms / 1000);
-  m = Math.floor(s / 60);
-  s = s % 60;
-  h = Math.floor(m / 60);
-  m = m % 60;
-  d = Math.floor(h / 24);
-  h = h % 24;
-  
-  this.days = d;
-  this.hours = h;
-  this.minutes = m;
-  this.seconds = s;
-}
+  function DateMeasure(ms) {
+    var d, h, m, s;
+    s = Math.floor(ms / 1000);
+    m = Math.floor(s / 60);
+    s = s % 60;
+    h = Math.floor(m / 60);
+    m = m % 60;
+    d = Math.floor(h / 24);
+    h = h % 24;
+    
+    this.days = d;
+    this.hours = h;
+    this.minutes = m;
+    this.seconds = s;
+  };
+
+  Date.diff = function(date1, date2) {
+    return new DateDiff(date1, date2);
+  };
+
+  Date.prototype.diff = function(date2) {
+    return new DateDiff(this, date2);
+  };
+
+})();
