364161bc1902f36ef3a08b26898685d662c656c3
AE Creations <aecreations@users.noreply.github.com>
2021-05-02 23:03:51 -0700

    Moved event handler definitions out of initialization function

--- wx-src/background.js
+++ wx-src/background.js
@@ -510,17 +510,10 @@ function init()
       gPrefs.clippingsMgrMinzWhenInactv = (gOS == "linux");
     }
 
-    browser.browserAction.onClicked.addListener(aTab => {
-      openClippingsManager();
-    });
-
     gClippingsListener.origin = aeConst.ORIGIN_HOSTAPP;
     gClippingsListeners.add(gClippingsListener);
     gSyncClippingsListeners.add(gSyncClippingsListener);
     
-    window.addEventListener("unload", onUnload, false);
-    initMessageListeners();
-
     if (gPrefs.syncClippings) {
       gSyncFldrID = gPrefs.syncFolderID;
 
@@ -534,33 +527,6 @@ function init()
     
     aeClippingSubst.init(navigator.userAgent, gPrefs.autoIncrPlcHldrStartVal);
     gAutoIncrPlchldrs = new Set();
-
-    browser.storage.onChanged.addListener((aChanges, aAreaName) => {
-      let changedPrefs = Object.keys(aChanges);
-
-      for (let pref of changedPrefs) {
-        gPrefs[pref] = aChanges[pref].newValue;
-
-        if (pref == "autoIncrPlcHldrStartVal") {
-          aeClippingSubst.setAutoIncrementStartValue(aChanges[pref].newValue);
-        }
-        else if (gPrefs.pasteShortcutKeyPrefix && !isDirectSetKeyboardShortcut()) {
-          setShortcutKeyPrefix(gPrefs.pasteShortcutKeyPrefix);
-        }
-      }
-    });
-
-    browser.commands.onCommand.addListener(async (aCmdName) => {
-      info(`Clippings/wx: Command "${aCmdName}" invoked!`);
-
-      if (aCmdName == aeConst.CMD_CLIPPINGS_KEYBOARD_PASTE && gPrefs.keyboardPaste) {
-        let tabs = await browser.tabs.query({ active: true, currentWindow: true });
-        let activeTabID = tabs[0].id;
-        gPasteClippingTargetTabID = activeTabID;
-        log(`Clippings/wx: Active tab ID: ${activeTabID} - opening keyboard paste dialog.`);
-        openKeyboardPasteDlg();
-      }
-    });
 
     if (gPrefs.pasteShortcutKeyPrefix && !isDirectSetKeyboardShortcut()) {
       setShortcutKeyPrefix(gPrefs.pasteShortcutKeyPrefix);
@@ -808,76 +774,6 @@ function purgeFolderItems(aFolderID, aKeepFolder)
     }).catch(aErr => {
       aFnReject(aErr);
     });
-  });
-}
-
-
-function initMessageListeners()
-{
-  browser.runtime.onMessage.addListener(aRequest => {
-    log(`Clippings/wx: Received message "${aRequest.msgID}"`);
-    
-    let resp = null;
-
-    if (aRequest.msgID == "init-new-clipping-dlg") {
-      resp = gNewClipping.get();
-
-      if (resp !== null) {
-        resp.saveSrcURL = gPrefs.alwaysSaveSrcURL;
-        resp.checkSpelling = gPrefs.checkSpelling;
-        return Promise.resolve(resp);
-      }
-    }
-    else if (aRequest.msgID == "init-placeholder-prmt-dlg") {
-      resp = gPlaceholders.get();
-      return Promise.resolve(resp);
-    }
-    else if (aRequest.msgID == "close-new-clipping-dlg") {
-      gWndIDs.newClipping = null;
-    }
-    else if (aRequest.msgID == "close-clippings-mgr-wnd") {
-      gWndIDs.clippingsMgr = null;
-    }
-    else if (aRequest.msgID == "close-keybd-paste-dlg") {
-      gWndIDs.keyboardPaste = null;
-    }
-    else if (aRequest.msgID == "paste-shortcut-key") {
-      let shortcutKey = aRequest.shortcutKey;
-      if (! shortcutKey) {
-        return;
-      }
-
-      log(`Clippings/wx: Key '${shortcutKey}' was pressed.`);
-      pasteClippingByShortcutKey(shortcutKey);
-    }
-
-    else if (aRequest.msgID == "paste-clipping-by-name") {
-      let externReq = aRequest.fromClippingsMgr;
-      pasteClippingByID(aRequest.clippingID, externReq);
-    }
-    else if (aRequest.msgID == "paste-clipping-with-plchldrs") {
-      let content = aRequest.processedContent;
-
-      window.setTimeout(async () => {
-        let tabs = await browser.tabs.query({active: true, currentWindow: true});
-        if (! tabs[0]) {
-          // This could happen if the browser tab was closed while the
-          // placeholder prompt dialog was open.
-          alertEx(gMsgBoxStr.MSG_NO_ACTIVE_BROWSER_TAB);
-          return;
-        }
-
-        let activeTabID = tabs[0].id;
-        if (activeTabID != gPasteClippingTargetTabID) {
-          warn(`Clippings/wx: Detected mismatch between currently-active browser tab ID and what it was when invoking clipping paste.\nPrevious active tab ID = ${gPasteClippingTargetTabID}, active tab ID = ${activeTabID}`);
-          activeTabID = gPasteClippingTargetTabID;
-        }
-        pasteProcessedClipping(content, activeTabID);
-      }, 60);
-    }
-    else if (aRequest.msgID == "close-placeholder-prmt-dlg") {
-      gWndIDs.placeholderPrmt = null;
-    }
   });
 }
 
@@ -1737,12 +1633,6 @@ function isDirectSetKeyboardShortcut()
 }
 
 
-function onUnload(aEvent)
-{
-  gClippingsListeners.remove(gClippingsListener);
-}
-
-
 function showSyncErrorNotification()
 {
   browser.notifications.create(aeConst.NOTIFY_SYNC_ERROR_ID, {
@@ -1905,8 +1795,26 @@ function versionCompare(aVer1, aVer2)
 
 
 //
-// Click event listener for the context menu items
+// Event handlers
 //
+
+browser.browserAction.onClicked.addListener(aTab => {
+  openClippingsManager();
+});
+
+
+browser.commands.onCommand.addListener(async (aCmdName) => {
+  info(`Clippings/wx: Command "${aCmdName}" invoked!`);
+
+  if (aCmdName == aeConst.CMD_CLIPPINGS_KEYBOARD_PASTE && gPrefs.keyboardPaste) {
+    let tabs = await browser.tabs.query({ active: true, currentWindow: true });
+    let activeTabID = tabs[0].id;
+    gPasteClippingTargetTabID = activeTabID;
+    log(`Clippings/wx: Active tab ID: ${activeTabID} - opening keyboard paste dialog.`);
+    openKeyboardPasteDlg();
+  }
+});
+
 
 browser.contextMenus.onClicked.addListener(async (aInfo, aTab) => {
   switch (aInfo.menuItemId) {
@@ -1976,10 +1884,6 @@ browser.contextMenus.onClicked.addListener(async (aInfo, aTab) => {
 });
 
 
-//
-// Click event listener for notifications
-//
-
 browser.notifications.onClicked.addListener(aNotifID => {
   if (aNotifID == aeConst.NOTIFY_BACKUP_REMIND_ID) {
     // Open Clippings Manager in backup mode.
@@ -1993,6 +1897,94 @@ browser.notifications.onClicked.addListener(aNotifID => {
   }
 });
   
+
+browser.storage.onChanged.addListener((aChanges, aAreaName) => {
+  let changedPrefs = Object.keys(aChanges);
+
+  for (let pref of changedPrefs) {
+    gPrefs[pref] = aChanges[pref].newValue;
+
+    if (pref == "autoIncrPlcHldrStartVal") {
+      aeClippingSubst.setAutoIncrementStartValue(aChanges[pref].newValue);
+    }
+    else if (gPrefs.pasteShortcutKeyPrefix && !isDirectSetKeyboardShortcut()) {
+      setShortcutKeyPrefix(gPrefs.pasteShortcutKeyPrefix);
+    }
+  }
+});
+
+
+browser.runtime.onMessage.addListener(aRequest => {
+  log(`Clippings/wx: Received message "${aRequest.msgID}"`);
+  
+  let resp = null;
+
+  if (aRequest.msgID == "init-new-clipping-dlg") {
+    resp = gNewClipping.get();
+
+    if (resp !== null) {
+      resp.saveSrcURL = gPrefs.alwaysSaveSrcURL;
+      resp.checkSpelling = gPrefs.checkSpelling;
+      return Promise.resolve(resp);
+    }
+  }
+  else if (aRequest.msgID == "init-placeholder-prmt-dlg") {
+    resp = gPlaceholders.get();
+    return Promise.resolve(resp);
+  }
+  else if (aRequest.msgID == "close-new-clipping-dlg") {
+    gWndIDs.newClipping = null;
+  }
+  else if (aRequest.msgID == "close-clippings-mgr-wnd") {
+    gWndIDs.clippingsMgr = null;
+  }
+  else if (aRequest.msgID == "close-keybd-paste-dlg") {
+    gWndIDs.keyboardPaste = null;
+  }
+  else if (aRequest.msgID == "paste-shortcut-key") {
+    let shortcutKey = aRequest.shortcutKey;
+    if (! shortcutKey) {
+      return;
+    }
+
+    log(`Clippings/wx: Key '${shortcutKey}' was pressed.`);
+    pasteClippingByShortcutKey(shortcutKey);
+  }
+
+  else if (aRequest.msgID == "paste-clipping-by-name") {
+    let externReq = aRequest.fromClippingsMgr;
+    pasteClippingByID(aRequest.clippingID, externReq);
+  }
+  else if (aRequest.msgID == "paste-clipping-with-plchldrs") {
+    let content = aRequest.processedContent;
+
+    window.setTimeout(async () => {
+      let tabs = await browser.tabs.query({active: true, currentWindow: true});
+      if (! tabs[0]) {
+        // This could happen if the browser tab was closed while the
+        // placeholder prompt dialog was open.
+        alertEx(gMsgBoxStr.MSG_NO_ACTIVE_BROWSER_TAB);
+        return;
+      }
+
+      let activeTabID = tabs[0].id;
+      if (activeTabID != gPasteClippingTargetTabID) {
+        warn(`Clippings/wx: Detected mismatch between currently-active browser tab ID and what it was when invoking clipping paste.\nPrevious active tab ID = ${gPasteClippingTargetTabID}, active tab ID = ${activeTabID}`);
+        activeTabID = gPasteClippingTargetTabID;
+      }
+      pasteProcessedClipping(content, activeTabID);
+    }, 60);
+  }
+  else if (aRequest.msgID == "close-placeholder-prmt-dlg") {
+    gWndIDs.placeholderPrmt = null;
+  }
+});
+
+
+window.addEventListener("unload", aEvent => {
+  gClippingsListeners.remove(gClippingsListener);
+});
+
 
 //
 // Catch any unhandled promise rejections from 3rd-party libs
