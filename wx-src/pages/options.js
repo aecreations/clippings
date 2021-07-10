a6d1c1635f4bbaa4cfe17ea8ca9d533ce4524dd5
AE Creations <aecreations@users.noreply.github.com>
2021-07-10 08:56:13 -0700

    Localized the UI string for macOS

--- wx-src/pages/options.js
+++ wx-src/pages/options.js
@@ -495,7 +495,7 @@ function initDialogs()
 
     if (! that.isInitialized) {
       if (gOS != "win") {
-        let os = gOS == "mac" ? "macOS" : capitalize(gOS);
+        let os = gOS == "mac" ? browser.i18n.getMessage("macOS") : capitalize(gOS);
         $("#wnds-dlgs-opts-dlg").css({height: "320px"});
         $("#wnds-dlgs-opts-exp-warn-msg").text(browser.i18n.getMessage("wndsDlgsOptsExpWarn", os));
         $("#wnds-dlgs-opts-exp-warn").show();
