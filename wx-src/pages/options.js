a9a27901f9972d44d51c4a3266a85cf15d5e9974
AE Creations <aecreations@users.noreply.github.com>
2021-09-29 17:51:48 -0700

    Enabled ability to browse sync folder in Sync Clippings options dialog
    (ref: aecreations/sync-clippings#10)
    - Requires Sync Clippings 1.2

--- wx-src/pages/options.js
+++ wx-src/pages/options.js
@@ -189,20 +189,19 @@ async function init()
   $("#sync-settings").click(aEvent => {
     gDialogs.syncClippings.showModal();
   });
-  /***
-      $("#browse-sync-fldr").click(aEvent => {
-      let msg = { msgID: "sync-dir-folder-picker" };
-      let sendNativeMsg = browser.runtime.sendNativeMessage(aeConst.SYNC_CLIPPINGS_APP_NAME, msg);
 
-      sendNativeMsg.then(aResp => {
+  $("#browse-sync-fldr").click(aEvent => {
+    let msg = { msgID: "sync-dir-folder-picker" };
+    let sendNativeMsg = browser.runtime.sendNativeMessage(aeConst.SYNC_CLIPPINGS_APP_NAME, msg);
+
+    sendNativeMsg.then(aResp => {
       if (aResp.syncFilePath) {
-      $("#sync-fldr-curr-location").val(aResp.syncFilePath);
+        $("#sync-fldr-curr-location").val(aResp.syncFilePath);
       }
-      }).catch(aErr => {
+    }).catch(aErr => {
       window.alert("The Sync Clippings helper app responded with an error.\n\n" + aErr);
-      });
-      });
-  ***/
+    });
+  });
   
   $("#show-sync-help").click(aEvent => {
     gDialogs.syncClippingsHelp.showModal();
@@ -320,6 +319,11 @@ function initDialogs()
 
     sendNativeMsg.then(aResp => {
       console.info("Sync Clippings helper app version: " + aResp.appVersion);
+
+      if (gClippings.versionCompare(aResp.appVersion, "1.2b1") < 0) {
+        $("#browse-sync-fldr").hide();
+      }
+      
       return aePrefs.getAllPrefs();
 
     }).then(aPrefs => {
