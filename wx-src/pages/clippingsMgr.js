38ba5064bc1b25b37152bddf847c6755b7fe2719
AE Creations <aecreations@users.noreply.github.com>
2022-06-23 19:28:31 -0700

    Missing semicolon

--- wx-src/pages/clippingsMgr.js
+++ wx-src/pages/clippingsMgr.js
@@ -4438,7 +4438,7 @@ function initDialogs()
     setTimeout(() => {
       $("#remove-all-source-urls-dlg > .dlg-btns > .dlg-accept")[0].focus();
     }, 10);
-  }
+  };
 
   gDialogs.removeAllSrcURLsConfirm = new aeDialog("#all-src-urls-removed-confirm-msgbar");
   gDialogs.removeAllSrcURLsConfirm.onInit = function ()
