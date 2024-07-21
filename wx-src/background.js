13198e81ad5195c248e68e5307d4e1f09e6d090a
AE Creations <aecreations@users.noreply.github.com>
2024-07-21 12:31:52 -0700

    Adjusted height of the Clippings Sidebar help window

--- wx-src/background.js
+++ wx-src/background.js
@@ -1613,11 +1613,11 @@ async function openBackupDlg()
 async function openSidebarHelpDlg()
 {
   let url = browser.runtime.getURL("pages/sidebarHelp.html");
-  let height = 360;
+  let height = 376;
   let platform = await browser.runtime.getPlatformInfo();
 
   if (platform.os == "mac") {
-    height = 334;
+    height = 360;
   }
   
   let wndPpty = {
